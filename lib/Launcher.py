da731c294c3f718c4080a87d597d0fbfa369e227
Samih Ouague <ouaguenouni.samih@gmail.com>
2022-05-14 03:00:51 +0200

    Rewriting the bot

--- lib/Launcher.py
+++
@@ -1,5 +0,0 @@
-from lib.Prototype import Prototype
-
-class Launcher:
-    def __init__(self, symbol, initialCandles, takeProfit, stopLoss, priceActionSell=False, priceActionBuy=False):
-        self.prototype = Prototype(symbol, initialCandles, priceActionBuy, priceActionSell, takeProfit, stopLoss)
